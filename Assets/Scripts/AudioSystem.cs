5a0986414b1faa5d5df7e5121a007ee4fc7712e4
PhilipN <philip.newsham@hotmail.co.uk>
2018-05-13 01:28:21 +0100

    added rain setting

--- Assets/Scripts/AudioSystem.cs
+++ Assets/Scripts/AudioSystem.cs
@@ -7,20 +7,25 @@ using UnityEngine.Audio;
 public class AudioSystem : MonoBehaviour
 {
     public Button[] audioButtons;
-    private Text[] audioButtonTexts = new Text[2];
+    private Text[] audioButtonTexts;
 
     public AudioMixerSnapshot[] musicSnapshots;
     public AudioMixerSnapshot[] sfxSnapshots;
+    public AudioMixerSnapshot[] rainSnapshots;
 
     private int music;
     private int sfx;
+    private int rain;
 
 	void Start ()
     {
         audioButtons[0].onClick.AddListener(() => MuteMusic());
         audioButtons[1].onClick.AddListener(() => MuteSFX());
-        audioButtonTexts[0] = audioButtons[0].GetComponentInChildren<Text>();
-        audioButtonTexts[1] = audioButtons[1].GetComponentInChildren<Text>();
+        audioButtons[2].onClick.AddListener(() => MuteRain());
+
+        audioButtonTexts = new Text[audioButtons.Length];
+        for (int i = 0; i < audioButtons.Length; i++)
+            audioButtonTexts[i] = audioButtons[i].GetComponentInChildren<Text>();
 	}
 	
 	void MuteMusic()
@@ -35,5 +40,12 @@ public class AudioSystem : MonoBehaviour
         sfx = (sfx + 1) % 2;
         sfxSnapshots[sfx].TransitionTo(0.0f);
         audioButtonTexts[1].text = string.Format("SFX: {0}", sfx == 0 ? "On" : "Off");
+    }
+
+    void MuteRain()
+    {
+        rain = (rain + 1) % 2;
+        rainSnapshots[rain].TransitionTo(0.0f);
+        audioButtonTexts[2].text = string.Format("Rain: {0}", rain == 0 ? "On" : "Off");
     }
 }
